c66bca6b336bbe550b04e968d642e28a7e4f062d
Emir <emirhansirkeci8@gmail.com>
2023-09-09 15:19:44 +0300

    Change vercel config file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const axios = require("axios");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const url =
   "https://ipfs.io/ipfs/bafybeibo2oniklnbpcyrciqrserby3qxnug27mvzxan7z4xrsdway6pijq/";
